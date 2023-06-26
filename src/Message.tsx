acff1fb2f58a998d66462b5b4b033abaddbd3b0f
Afsar Shaikh <shaikhafsar@hotmail.com>
2023-06-26 19:13:10 +0200

    Getting Started with React

--- src/Message.tsx
+++ src/Message.tsx
@@ -3,7 +3,7 @@ function Message() {
   //JSX: Javascript XML
   const name = "Afsar";
   if (name) return <h1>Hello {name}!</h1>;
-  return <h1>Hello World!</h1>;
+  return <h1>Hello World !</h1>;
 }
 
 export default Message;
